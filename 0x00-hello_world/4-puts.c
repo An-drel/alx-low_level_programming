47a5a4786955fe62cd5265dc4aaf77a9ef3aca97
An-drel <andrelefe33@gmail.com>
2022-10-15 08:10:25 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char an_drel[] = "\"Programming is like building a multilingual puzzle"
+	char an_drel[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(an_drel);
 	return (0);
